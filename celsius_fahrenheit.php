689e7105c9c32803428f271a6200a88669508b7a
ejmulislam <mdejmulislam62@gmail.com>
2024-07-05 12:04:58 +0600

    Celsius To Fahrenheit Done

---
+++ celsius_fahrenheit.php
@@ -0,0 +1,9 @@
+<?php
+
+$celsius = 32;
+
+// Celsius to Fahrenheit Conversion 
+$fahrenheit = ($celsius * 9/5) + 32;
+
+//printf function to show up to 2 decimal points.
+echo printf('%.1f', $fahrenheit) . "F";
